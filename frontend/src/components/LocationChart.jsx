2a07000837008199de0ce489111effd485fa57b5
Swap9035 <swapnilpatil9035@gmail.com>
2026-03-31 22:06:39 +0530

    fix: removed unused Legend import

--- frontend/src/components/LocationChart.jsx
+++ frontend/src/components/LocationChart.jsx
@@ -3,8 +3,7 @@
 
 import React from 'react';
 import {
-  PieChart, Pie, Cell, Tooltip,
-  Legend, ResponsiveContainer
+  PieChart, Pie, Cell, Tooltip, ResponsiveContainer
 } from 'recharts';
 import useApi from '../hooks/useApi';
 
